70bc8c6350fa8dbad8fa214c964c8857ece3393c
jrc03c <jrc03c@pm.me>
2022-06-25 12:47:59 -0500

    ...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const gt = {
   program: require("./src/program"),
   job: require("./src/job"),
   request: require("./src/request"),
-  common: require("./src/common.js"),
+  common: require("./src/common"),
   helpers: require("./src/helpers.js"),
 }
 
@@ -10,9 +10,8 @@ module.exports = gt
 
 if (require.main === module) {
   async function run() {
-    const id = 19923
-    const contents = await gt.program.getContents(id)
-    console.log(contents)
+    const config = await gt.common.config.load()
+    config.save()
   }
 
   run()
