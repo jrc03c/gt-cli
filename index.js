104897306b1d8c30c26642e5d8b098954a34bc05
jrc03c <jrc03c@pm.me>
2022-06-25 13:06:25 -0500

    ...

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ const gt = {
 module.exports = gt
 
 if (require.main === module) {
+  function makeKey(n) {
+    let out = ""
+    const alpha = "0123456789abcdef"
+    while (out.length < n) out += alpha[parseInt(Math.random() * alpha.length)]
+    return out
+  }
+
   async function run() {
-    const config = await gt.common.config.load()
-    config.save()
+    const secrets = require("./secrets.json")
+    const config = await gt.common.config.load(secrets)
+    const data = await gt.program.create(makeKey(32))
+    console.log(data)
   }
 
   run()
